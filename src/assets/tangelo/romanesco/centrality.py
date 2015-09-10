f07a6aa6f1152932920cf6f99975f937dddbc412
Roni Choudhury <roni.choudhury@kitware.com>
2015-09-10 15:04:53 -0400

    Filled in centrality computation via romanesco

--- src/assets/tangelo/romanesco/centrality.py
+++ src/assets/tangelo/romanesco/centrality.py
@@ -1,12 +1,39 @@
-import bson.json_util
+import tangelo
 
+import json
+import networkx as nx
+import romanesco
+
+betweenness_centrality = {
+    'inputs': [
+        {'name': 'G',
+         'type': 'graph',
+         'format': 'networkx'},
+        {'name': 'node',
+         'type': 'string',
+         'format': 'text'}
+    ],
+    'outputs': [
+        {'name': 'measure',
+         'type': 'number',
+         'format': 'number'}
+    ],
+    'script':
+'''
+from networkx import betweenness_centrality
+measure = betweenness_centrality(G)[node]
+'''
+}
 
 def run(node=None, graph=None):
     """Computes the centrality of ``node`` (given by key) within the subgraph
     represented by ``graph`` (which is in Clique-Mongo format."""
 
-    graph = bson.json_util.loads(graph)
+    tangelo.log_warning(type(node))
+    tangelo.log_warning(type(graph))
 
-    # TODO: compute centrality here.
-
-    return 3.14159
+    output = romanesco.run(betweenness_centrality, inputs={'G': {'format': 'clique.json',
+                                                                 'data': graph},
+                                                           'node': {'format': 'text',
+                                                                    'data': node}})
+    return output["measure"]["data"]
